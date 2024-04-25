d6e4d5374dafa0e0595c588dc2e61073ac5d5bfe
sim-07 <121951559+sim-07@users.noreply.github.com>
2024-04-25 21:10:51 +0200

    Update installpy.bat

--- installpy.bat
+++ installpy.bat
@@ -1,4 +1,3 @@
-curl -o python-installer.exe https://www.python.org/ftp/python/3.x.x/python-3.9.9-amd64.exe
-
+curl -o python-installer.exe https://www.python.org/ftp/python/3.9.9/python-3.9.9-amd64.exe
 python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 
